d7e63706d39aa1b14a1dd084901de85a91e81ead
Fabian <oraze.fabian@icloud.com>
2022-10-22 17:28:57 +0200

    improved is_sorted function, now is should run in O(N)

--- src/sorting/utils_sort.h
+++ src/sorting/utils_sort.h
@@ -1,12 +1,12 @@
 #ifndef UTILS_SORT
 #define UTILS_SORT
 
-
 /**
  * Swap 2 elements. Implements XOR-Swap
  * @see https://en.wikipedia.org/wiki/XOR_swap_algorithm#Code_example
- */ 
-void swap(int* x, int* y){
+ */
+void swap(int *x, int *y)
+{
     int t = *x;
     *x = *y;
     *y = t;
@@ -18,18 +18,22 @@ void swap(int* x, int* y){
  * @param n as array lenght
  * @return 1 if sorted, 0 if not sorted
  */
-int is_sorted(int arr[], int n){
+int is_sorted(int arr[], int n)
+{
 
-    for(int i = 1; i < n; i++){
-        for(int j = 0; j < i; j++){
-            if(arr[j] > arr[i]){
-                return 0;
-            }
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+        {
+            return 1;
+        }
+
+        if (arr[i + 1] < arr[i])
+        {
+            return 0;
         }
     }
     return 1;
 }
-
-
 
 #endif
