ae1ce6f5472c87d95f76aee6a53bba058d0b5927
Fabian <oraze.fabian@icloud.com>
2022-12-30 12:20:07 +0100

    added parallel insertion sort, but effectively it evolved into a variant of bucket sort

--- src/performance_analytics/sorting/omp_insertion_sort.h
+++ src/performance_analytics/sorting/omp_insertion_sort.h
@@ -8,22 +8,75 @@
 #define OMP_THREADS 2
 #endif
 
+// insertion sort effectively evolves into a bucket sort variant
+
 /**
  * Implementation for simple insertion sort using OpenMP Pragmas
- * TODO
  * @param arr as array to sort
  * @param n as amount of elements in the array
  */
-void omp_insertion_sort(int arr[], int n){
-    int cur_el, j;
-    for(int i = 1; i < n; i++){
-        cur_el = arr[i];
+void omp_insertion_sort(int arr[], int n)
+{
+    int bucket_count = OMP_THREADS;
+    int bucket_interval = n / bucket_count;
 
-        for(j = i-1; j >= 0 && arr[j] > cur_el; ){
-            arr[j+1] = arr[j];
-            j = j-1;
+    // Increase offset in order not to lose any values
+    while (bucket_count * bucket_interval < n)
+    {
+        bucket_interval += 1;
+    }
+
+    int buckets[bucket_count][n]; // how should be best assign the size of each bucket??
+    int bucket_index_count[bucket_count];
+    for (int i = 0; i < bucket_count; i++)
+        bucket_index_count[i] = 0;
+
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    max++;
+
+    // printf("bucket_count: %d,  max: %d\n", bucket_count, max);
+    //  Create buckets
+
+    for (int i = 0; i < n; i++)
+    {
+        int bucket_index = (bucket_count * arr[i]) / (max);
+        buckets[bucket_index][bucket_index_count[bucket_index]] = arr[i];
+        bucket_index_count[bucket_index]++;
+        ////printf("Inserting value  %d, into bucket %d, bucket size new: %d\n", arr[i], ((bucket_count * arr[i]) / (max)), bucket_index_count[bucket_index]);
+    }
+
+    int cur_el, j;
+    #pragma omp parallel for schedule(static) private(cur_el,j)
+    for (int b = 0; b < bucket_count; b++)
+    {
+        for (int i = 1; i < bucket_index_count[b]; i++)
+        {
+            cur_el = buckets[b][i]; 
+
+            j = i - 1;
+            while (j >= 0 && buckets[b][j] > cur_el)
+            {                        
+                buckets[b][j + 1] = buckets[b][j]; 
+                j = j - 1;
+            }
+
+            buckets[b][j + 1] = cur_el; 
         }
-        arr[j+1] = cur_el;
+    }
+
+    int arr_index = 0;
+    for (int i = 0; i < bucket_count; i++)
+    {
+        for (int j = 0; j < bucket_index_count[i]; j++) 
+        {
+            arr[arr_index] = buckets[i][j];
+            arr_index++;
+        }
     }
 }
 
